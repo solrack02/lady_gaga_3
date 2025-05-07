55c39ed08593f6953c29891090e6874f0f79568b
Carlos A. <carlosvsk@gmail.com>
2025-05-07 11:37:35 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -175,7 +175,7 @@ xmlns="http://www.w3.org/2000/svg"
               }
               ],
 
-      URIvariablePath:["https://images.unsplash.com/photo-1488831861984-179da3647265?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+      URIvariablePath:[`https://4kwallpapers.com/images/wallpapers/lady-gaga-1920x1200-16726.jpeg`],
 
       args,
     }}/>],
